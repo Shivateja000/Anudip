c23542a4b9c0309ee895239c5eaa64e5cb2bea56
Shivateja000 <shivatejagoud152@gmail.com>
2024-06-12 09:20:09 +0530

    Lab-2

---
+++ Lab2/UserLoginCheck.java
@@ -0,0 +1,14 @@
+package Samplepackage;
+
+public class UserLoginCheck {
+
+	    public static void main(String[] args){
+	         String login = "teja";
+	         if(login == "shiva") {
+	        	 System.out.println("user is valid");
+	         }
+	         else {
+	        	 System.out.println("user is not logged in");
+	         }
+	    }
+}
